1acac888b7cce5da5240d362df491efabf3338ba
Gabriel Paranhos <gbrparanhos@gmail.com>
2026-05-17 01:47:20 -0300

    atualiza scripts de deploy

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -1,31 +1,34 @@
 set -e
 
-if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
-  echo "O arquivo deve ser executado da seguinte forma: ./deploy.sh <REMOTE_USER> <REMOTE_HOST> <SSH_PORT>"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "O arquivo deve ser executado da seguinte forma: ./deploy.sh <REMOTE_USER> <REMOTE_HOST>"
   exit 1
 fi
 
 REMOTE_USER=$1
 REMOTE_HOST=$2
-SSH_PORT=$3
 APP_NAME="my-finance-api-dev"
 
 echo "🚀 Iniciando deploy..."
 
-rsync -avz --delete --relative --progress --exclude 'node_modules' --exclude 'dist' -e "ssh -p ${SSH_PORT}" ./ ${REMOTE_USER}@${REMOTE_HOST}:~/${APP_NAME}
+rsync -avz --delete --relative --progress --exclude 'node_modules' --exclude 'dist' \
+  -e "ssh -o ProxyCommand='cloudflared access ssh --hostname ${REMOTE_HOST}'" \
+  ./ ${REMOTE_USER}@${REMOTE_HOST}:~/${APP_NAME}
 
-ssh -T -p ${SSH_PORT} ${REMOTE_USER}@${REMOTE_HOST} << EOF
+ssh -T \
+  -o "ProxyCommand=cloudflared access ssh --hostname ${REMOTE_HOST}" \
+  ${REMOTE_USER}@${REMOTE_HOST} << EOF
   set -e
   cd ~/${APP_NAME}
 
   echo "📦 Instalando dependências..."
   npm ci
 
-  echo "📑 Gerando Prisma Client..."
-  npx prisma generate
-
   echo "🏗️  Buildando projeto..."
   npm run build
+
+  echo "📑 Rodando migrations..."
+  npx mikro-orm migration:up
 
   echo "🔍 Verificando se o app já está rodando no PM2..."
   if pm2 describe ${APP_NAME} > /dev/null 2>&1; then
